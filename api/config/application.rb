d65910566514fc25c34be1906346e6b038f452f4
Austin Sprawls <austinsprawls@gmail.com>
2014-12-19 11:37:59 -0600

    configured rack cors in api module

--- api/config/application.rb
+++ api/config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
+require "rails/all"
 require "active_model/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
@@ -26,5 +27,12 @@ module Api
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.middleware.use Rack:Cors do
+      allow do
+        origins "*"
+        resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options]
+      end
+    end
   end
 end
