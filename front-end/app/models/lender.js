bf8bd5a34b5b73b126e55a4863875109d71f68ed
Austin Sprawls <austinsprawls@gmail.com>
2014-12-22 14:23:25 -0600

    added lender model to ember data

---
+++ front-end/app/models/lender.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  email: DS.attr('string'),
+  password: DS.attr('string'),
+  passwordConfirmation: DS.attr('string'),
+  stateOfResidence: DS.attr('string'),
+  funds: DS.attr({defaultValue: 0.0}),
+  totalInvested: DS.attr({defaultValue: 0.0})
+});
