d6662de4d74298fa0048d4e6f3621a6340e14d6b
Austin Sprawls <austinsprawls@gmail.com>
2014-12-22 12:50:16 -0600

    used factory girl to create more mock data for models

--- api/spec/factories/lenders.rb
+++ api/spec/factories/lenders.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :lender do
-    
+    email {Faker::Internet.email}
+    password {"password"}
+    password_confirmation {"password"}
+    state_of_residence {Faker::Address.state}
   end
 
 end
--- api/spec/factories/payments.rb
+++ api/spec/factories/payments.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :payment do
-    
+    lender
+    lendee
+    amount {Faker::Commerce.price}
   end
 
 end
