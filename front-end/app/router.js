91d60a5e1803c39a4a151a6d1649f739321e0680
Austin Sprawls <austinsprawls@gmail.com>
2014-12-23 11:10:02 -0600

    further refactored router.js

--- front-end/app/router.js
+++ front-end/app/router.js
@@ -9,17 +9,21 @@ Router.map(function() {
   this.resource('contacts', function(){
     this.resource('contact', {path: '/:contact_id'});
   });
-  this.resource('lendee', { path: 'lendees/:lendee_id' });
-  this.resource('lendee.payments', {path: 'lendees/:lendee_id/payments'} ,function(){
+  this.resource('lendee', { path: '/lendees/:lendee_id' });
+  this.resource('lendee.payments', {path: '/lendees/:lendee_id/payments'} ,function(){
     this.resource('lendee.payment', {path: '/:payment_id'});
   });
-  this.resource('lendee.investments', {path: 'lendees/:lendee_id/investments'}, function(){});
+  this.resource('lendee.investments', {path: '/lendees/:lendee_id/investments'}, function(){
+    this.resource('lendee.investment', {path: '/:investment_id'});
+  });
 
-  this.resource('lender', { path: 'lenders/:lender_id' }, function() {});
+  this.resource('lender', { path: 'lenders/:lender_id' });
   this.resource('lender.payments', {path: '/payments'}, function(){
     this.resource('lender.payment', {path: '/:payment_id'});
   });
-  this.resource('lender.investments', {path: '/investments'}, function(){});
+  this.resource('lender.investments', {path: '/investments'}, function(){
+    this.resource('lender.investment', {path: '/:investment_id'});
+  });
 });
 
 export default Router;
