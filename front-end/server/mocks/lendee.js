e163a313c2dd7fae93b75cb77f3ab2cbdf0c1e95
Austin Sprawls <austinsprawls@gmail.com>
2014-12-24 12:58:18 -0600

    wrapping up http-mock setup; about to merge with master

--- front-end/server/mocks/lendee.js
+++ front-end/server/mocks/lendee.js
@@ -2,6 +2,30 @@ module.exports = function(app) {
   var express = require('express');
   var lendeeRouter = express.Router();
 
+  var LENDEES = {
+    "10": {
+      id: 10,
+      src: 'http://media.giphy.com/media/UdqUo8xvEcvgA/giphy.gif'
+    },
+    "42": {
+      id: 42,
+      src: 'http://media0.giphy.com/media/Ko2pyD26RdYRi/giphy.gif'
+    }
+  };
+
+  var server = new Pretender(function(){
+    this.get('/photos', function(request){
+      var all =  JSON.stringify(Object.keys(PHOTOS).map(function(k){return PHOTOS[k]}))
+      return [200, {"Content-Type": "application/json"}, all]
+    });
+
+    this.get('/photos/:id', function(request){
+      return [200, {"Content-Type": "application/json"}, JSON.stringify(PHOTOS[request.params.id])]
+    });
+  });
+
+  $.get('/photos/12', {success: function(){ ... }})
+
   lendeeRouter.get('/', function(req, res) {
     res.send({
       "lendee": []
