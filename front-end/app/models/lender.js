05888ab75eb153c2453c798e27f2ae7b76511db0
Austin Sprawls <austinsprawls@gmail.com>
2014-12-30 10:58:59 -0600

    added five fixtures to lender model

--- front-end/app/models/lender.js
+++ front-end/app/models/lender.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+var Lender = DS.Model.extend({
   email: DS.attr('string'),
   password: DS.attr('string'),
   passwordConfirmation: DS.attr('string'),
@@ -12,3 +12,55 @@ export default DS.Model.extend({
   createdAt: DS.attr('date'),
   updatedAt: DS.attr('date')
 });
+
+Lender.reopenClass({
+  FIXTURES: [
+    {
+      id: 1,
+      email: "zion@runolfsdottir.net",
+      password: "password",
+      passwordConfirmation: "password",
+      stateOfResidence: "Oregon",
+      funds: 0,
+      totalInvested: 0
+    },
+    {
+      id: 2,
+      email: "woodrow_kemmer@grady.name",
+      password: "password",
+      passwordConfirmation: "password",
+      stateOfResidence: "South Carolina",
+      funds: 0,
+      totalInvested: 0
+    },
+    {
+      id: 3,
+      email: "emerson@schmelermarquardt.biz",
+      password: "password",
+      passwordConfirmation: "password",
+      stateOfResidence: "Arizona",
+      funds: 0,
+      totalInvested: 0
+    },
+    {
+      id: 4,
+      email:  "sadie@wisozkokeefe.info",
+      password: "password",
+      passwordConfirmation: "password",
+      stateOfResidence: "Wisconsin",
+      funds: 0,
+      totalInvested: 0
+    },
+    {
+      id: 5,
+      email:  "domingo@millskuhlman.name",
+      password: "password",
+      passwordConfirmation: "password",
+      stateOfResidence: "North Dakota",
+      funds: 0,
+      totalInvested: 0
+    }
+  ]
+});
+
+export default Lender;
