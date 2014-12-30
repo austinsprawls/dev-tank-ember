030f91874e6e05a82bc4fd5e8a2ce7d906a5274e
Austin Sprawls <austinsprawls@gmail.com>
2014-12-30 15:51:51 -0600

    working on integrating bootstrap modal functionality to invest in each loan

--- front-end/app/controllers/loan.js
+++ front-end/app/controllers/loan.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   percentageRate: function() {
     return Math.round(this.get("model.rate") * 100);
-  }.property("model.rate")
+  }.property("model.rate"),
+  modalId: function() {
+    return "invest-modal-" + this.get("model.id");
+  }.property("model.id"),
+  modalIdTarget: function(){
+    return "#invest-modal-" + this.get("model.id");
+  }.property("model.id")
 });
