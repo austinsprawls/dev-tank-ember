de12f18828c4b4f5292ffccbac0752d8d227117d
Austin Sprawls <austinsprawls@gmail.com>
2015-01-02 10:17:01 -0600

    finished with fixtures; about to move onto mocking login authorization with pretender

--- front-end/app/controllers/loans/index.js
+++ front-end/app/controllers/loans/index.js
@@ -1,5 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  
+  actions: {
+    sortByAmountRequested: function() {
+      this.set('sortProperties', ['amountRequested']);
+    },
+    sortByAmountRemaining: function() {
+      this.set('sortProperties', ['amountRemaining']);
+    },
+    sortByAmountFunded: function() {
+      this.set('sortProperties', ['amountFunded']);
+    },
+    sortByRate: function() {
+      this.set('sortProperties', ['rate']);
+    },
+    sortByTerm: function() {
+      this.set('sortProperties', ['term']);
+    }
+  }
 });
