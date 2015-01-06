6ea85257365bc16674e8e9a87ae5ae9466aaadd5
Austin Sprawls <austinsprawls@gmail.com>
2015-01-06 15:03:17 -0600

    restructured folders and files to reflect router

--- front-end/app/controllers/loan/investments/new.js
+++ front-end/app/controllers/loan/investments/new.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-});
+  actions: {
+    createInvestment: function(loan) {
+      var investmentAmount = this.get('investmentAmount');
+      if (!title.trim()) {return;}
+
+        var investment = this.store.createRecord('investment', {
+          lender: 1,
+          loan: loan.id,
+          amount: investmentAmount,
+          expectedReturn: investmentAmount*loan.rate
+        });
+
+        this.set('investmentAmount', '');
+
+        investment.save();
+
+        transitionToRoute('loans.index');
+      }
+    }
+  });
+  
--- front-end/app/routes/loan/investments/new.js
+++ front-end/app/routes/loan/investments/new.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
-  
+export default Ember.Route.extend(AuthenticatedRouteMixin,{
+  model: function(params){
+    console.log(params);
+    return this.store.find('loan', params.loan_id);
+  }
 });
